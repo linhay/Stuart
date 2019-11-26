1c68ee454bab17c2e3b95143289775c6f058b1c8
linhey <158179948@qq.com>
2019-11-26 20:50:32 +0800

    chore: sync

--- Sources/SectionView/SectionFlowLayout.swift
+++ Sources/SectionView/SectionFlowLayout.swift
@@ -1,25 +1,9 @@
 //
-//  Stuart
+//  SectionFlowLayout.swift
+//  iDxyer
 //
-//  github: https://github.com/linhay/Stuart
-//  Copyright (c) 2019 linhay - https://github.com/linhay
+//  Created by 林翰 on 2019/10/18.
 //
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 
 import UIKit
 
@@ -32,27 +16,42 @@ open class SectionFlowLayout: UICollectionViewFlowLayout {
     }
     
     public var contentMode = ContentMode.none
+
+    func modeLeft(collectionView: UICollectionView, attributes: [UICollectionViewLayoutAttributes]) -> [UICollectionViewLayoutAttributes]? {
+        var lineStore = [CGFloat: [UICollectionViewLayoutAttributes]]()
+        var list = [UICollectionViewLayoutAttributes]()
+
+        for item in attributes {
+            guard item.representedElementCategory == .cell,
+                let delegate = collectionView.delegate as? UICollectionViewDelegateFlowLayout,
+                /// self.minimumInteritemSpacing 获取时与 delegate 中数值不一致
+                let minimumInteritemSpacing = delegate.collectionView?(collectionView, layout: self, minimumInteritemSpacingForSectionAt: item.indexPath.section) else {
+                    list.append(item)
+                    continue
+            }
+
+            if let lastItem = lineStore[item.frame.minY]?.last {
+                item.frame.origin.x = lastItem.frame.maxX + minimumInteritemSpacing
+                lineStore[item.frame.minY]?.append(item)
+            } else {
+                lineStore[item.frame.minY] = [item]
+            }
+            list.append(item)
+        }
+        return list
+    }
     
     override open func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+
+        guard let attributes = super.layoutAttributesForElements(in: rect)?.map({ $0.copy() }) as? [UICollectionViewLayoutAttributes],
+
+            let collectionView = collectionView else {
+                return nil
+        }
+
         switch contentMode {
         case .left:
-            guard let attributes = super.layoutAttributesForElements(in: rect)?.map({ $0.copy() }) as? [UICollectionViewLayoutAttributes] else {
-                return nil
-            }
-            
-            var lineStore = [CGFloat: [UICollectionViewLayoutAttributes]]()
-            var list = [UICollectionViewLayoutAttributes]()
-            
-            for item in attributes {
-                if let lastItem = lineStore[item.frame.minY]?.last {
-                    item.frame.origin.x = lastItem.frame.maxX + minimumInteritemSpacing
-                    lineStore[item.frame.minY]?.append(item)
-                } else {
-                    lineStore[item.frame.minY] = [item]
-                }
-                list.append(item)
-            }
-            return list
+            return modeLeft(collectionView: collectionView, attributes: attributes)
         case .none:
             return super.layoutAttributesForElements(in: rect)
         }
--- Sources/SectionView/SectionManager.swift
+++ Sources/SectionView/SectionManager.swift
@@ -1,25 +1,9 @@
 //
+//  SectionManager.swift
 //  Stuart
 //
-//  github: https://github.com/linhay/Stuart
-//  Copyright (c) 2019 linhay - https://github.com/linhay
+//  Created by 林翰 on 2019/10/17.
 //
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 
 import UIKit
 
@@ -99,23 +83,23 @@ extension SectionManager: UICollectionViewDelegateFlowLayout {
     }
     
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return sections[section].headerSize
+        return sections[section].itemCount == 0 ? .zero : sections[section].headerSize
     }
     
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        return sections[section].footerSize
+        return sections[section].itemCount == 0 ? .zero : sections[section].footerSize
     }
     
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return sections[section].minimumLineSpacing
+        return sections[section].itemCount == 0 ? .zero : sections[section].minimumLineSpacing
     }
     
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return sections[section].minimumInteritemSpacing
+        return sections[section].itemCount == 0 ? .zero : sections[section].minimumInteritemSpacing
     }
     
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return sections[section].sectionInset
+        return sections[section].itemCount == 0 ? .zero : sections[section].sectionInset
     }
 }
 
